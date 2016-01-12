f156d474e316f246c3326ad2147a7a8975096a58
quakex <iquakex@gmail.com>
2016-01-12 10:43:20 +0800

    Initial commit - 手工打造 CRUD 应用程式

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,59 @@
+class EventsController < ApplicationController
+  #
+  before_action :set_event, :only => [:show, :edit, :update, :destroy]
+
+  # index 列出所有资料
+  def index
+    # 分页机制
+    @events = Event.page(params[:page]).per(5)
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to :action => :index
+      flash[:notice] = "event was successfully created"
+    else
+      render :action => :new
+    end
+  end
+
+  def show
+    @page_title = @event.name
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to :action => :show, :id => @event
+      flash[:notice] = "event was successfully created"
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+
+    flash[:alert] = "event was successfully deleted"
+    redirect_to :action => :index
+  end
+
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+end
